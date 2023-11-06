90c6b7975f3ebb11657d5555e30d18dd09414225
Alexey Tyurin <alexsei89tyrin@gmail.com>
2023-11-06 15:44:05 +0400

    Redux ToolKit и RTKQuery

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import styles from './App.module.scss'
 import { Login } from './Pages/Login/Login'
+import Test from './Pages/Test/Test'
 
 function App() {
 	return (
 		<div className={styles.container}>
-			<Login />
+			<Test />
 		</div>
 	)
 }
