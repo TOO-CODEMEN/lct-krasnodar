e83fe670f18d56c0882179d58dee080d1cce35ea
oliceglad <vycheslavofficial@gmail.com>
2023-11-07 13:16:19 +0400

    added states for support

--- src/Components/Support/Support.jsx
+++ src/Components/Support/Support.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './Support.module.scss'
 import { Button, TextField } from '@mui/material'
 
 export const Support = () => {
+    const [question, setQuestion] = useState({
+        name: '',
+        department: '',
+        desription: ''
+    })
+    const onSubmitHandler = (e) => {
+        e.preventDefault()
+        console.log(question)
+    }
+
+    const changeState = (event, typeObject) => {
+        setQuestion((value) => ({ ...value, [typeObject]: event.target.value }))
+    }
+
     return (
         <div className={styles.support}>
             <div className={styles.support__title}>
@@ -20,12 +34,29 @@ export const Support = () => {
                             style={{ marginRight: 30 }}
                             id="outlined"
                             label="Имя"
-                            type='name'
+                            type='text'
+                            defaultValue={question.name}
+                            onChange={(event) => changeState(event, 'name')}
                         />
-                        <TextField />
+                        <TextField
+                            required
+                            id="outlined"
+                            label="Отдел"
+                            type='text'
+                            defaultValue={question.department}
+                            onChange={(event) => changeState(event, 'department')}
+                        />
                     </div>
                     <div className={styles.support__flex__right__bottom}>
-                        <TextField style={{ width: '100%' }} />
+                        <TextField
+                            style={{ width: '100%' }}
+                            type='text'
+                            label='Вопрос'
+                            multiline
+                            minRows={5}
+                            defaultValue={question.desription}
+                            onChange={(event) => changeState(event, 'desription')}
+                        />
                         <Button
                             variant="contained"
                             style={{ borderRadius: 8, backgroundColor: '#F1367D', display: 'block', marginTop: 22 }}
