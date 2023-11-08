6b9cc8d7159221b39e503d07721f997aece332ac
oliceglad <vycheslavofficial@gmail.com>
2023-11-08 23:55:33 +0400

    added basic stata for user

--- src/Pages/Main/Main.jsx
+++ src/Pages/Main/Main.jsx
@@ -1,11 +1,13 @@
 import styles from './Main.module.scss'
 import Goal from './Goal/Goal'
 import UsefulMaterials from './UsefulMaterials/UsefulMaterials'
+import Statistics  from './Statistics/Statistics'
 
 export const Main = () => {
     return (
         <div className={styles.Main}>
             <Goal/>
+            <Statistics />
             <UsefulMaterials />
         </div>
     )
