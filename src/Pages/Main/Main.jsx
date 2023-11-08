84135a75edce4557a9edaaf1513fdb55e61ff96d
oliceglad <vycheslavofficial@gmail.com>
2023-11-08 23:03:46 +0400

    deleted support and imports in mainpage

--- src/Pages/Main/Main.jsx
+++ src/Pages/Main/Main.jsx
@@ -1,9 +1,6 @@
 import styles from './Main.module.scss'
 import Goal from './Goal/Goal'
 import UsefulMaterials from './UsefulMaterials/UsefulMaterials'
-import { Support } from './Support/Support'
-import { useGetTasksByUserIdQuery } from '../../api/tasks'
-import { useSelector } from 'react-redux'
 
 export const Main = () => {
     return (
@@ -11,7 +8,6 @@ export const Main = () => {
 
             <Goal/>
             <UsefulMaterials />
-            <Support />
         </div>
     )
 }
